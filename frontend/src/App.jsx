7a0a7b0a34c52ef0c5d232ae6bae68b44beffa69
Shaon CMD <shaoncmd@gmail.com>
2025-07-16 17:43:01 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Welcome from "./pages/Welcome.jsx";
 import AdminLogin from "./pages/AdminLogin.jsx";
 import UserLogin from "./pages/UserLogin.jsx";
+import Register from "./pages/Register.jsx"; // <--- Add this line
 import Dashboard from "./pages/Dashboard.jsx";
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Welcome />} />
         <Route path="/admin-login" element={<AdminLogin />} />
         <Route path="/user-login" element={<UserLogin />} />
+        <Route path="/register" element={<Register />} /> {/* Register Route */}
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
